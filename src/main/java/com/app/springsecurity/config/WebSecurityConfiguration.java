bc184beaa59437790c8a1ce10af8ebe4c1e7eae5
ramdhangar65 <ramdhangar65@gmail.com>
2022-01-29 15:04:09 +0530

    implemented spring-security with mysql and java-11

---
+++ src/main/java/com/app/springsecurity/config/WebSecurityConfiguration.java
@@ -0,0 +1,45 @@
+package com.app.springsecurity.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    UserDetailsService userDetailsService;
+
+    @Bean
+    AuthenticationProvider authenticationProvider() {
+
+        DaoAuthenticationProvider daoAuthenticationProvider =
+                new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(userDetailsService);
+        daoAuthenticationProvider.setPasswordEncoder(new BCryptPasswordEncoder());
+        return daoAuthenticationProvider;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/")
+                .permitAll()
+                .antMatchers("/home")
+                .hasAuthority("USER")
+                .antMatchers("/admin")
+                .hasAuthority("ADMIN")
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+}
---
+++ src/main/java/com/app/springsecurity/custom/CustomUserDetailsService.java
@@ -0,0 +1,25 @@
+package com.app.springsecurity.custom;
+
+import com.app.springsecurity.model.User;
+import com.app.springsecurity.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if(null==user)
+            throw new UsernameNotFoundException("User Not Found..");
+        return new CustomUserDetails(user);
+    }
+
+}
